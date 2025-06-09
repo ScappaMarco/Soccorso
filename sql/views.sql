724751eaa1e8308b83aed0b6e94b7932217ac58c
Noyz2000 <leonardocandeloro9@gmail.com>
2025-06-09 17:50:19 +0200

    implementazione op 11

--- sql/views.sql
+++ sql/views.sql
@@ -7,6 +7,7 @@ drop view if exists select_richieste_annullate;
 drop view if exists select_richieste_ignorate;
 drop view if exists select_richieste_in_corso;
 drop view if exists select_missioni_con_esito_non_totalmente_positivo;
+drop view if exists operatori_missioni_con_esito_non_totalmente_positivo;
 
 -- Operatori non occupati
 CREATE VIEW select_operatori_non_occupati AS
@@ -46,7 +47,16 @@ WHERE r.stato = 'in_corso';
 
 -- Missioni con esito non totalmente positivo
 CREATE VIEW select_missioni_con_esito_non_totalmente_positivo AS
-SELECT m.timestamp_inizio, m.obiettivo, c.livello_successo, c.timestamp_fine
+SELECT m.ID, m.ID_squadra, m.timestamp_inizio, m.obiettivo, c.livello_successo, c.timestamp_fine
 FROM conclusioni c
 JOIN missione m ON c.ID_missione = m.ID
 WHERE c.livello_successo < 5;
+
+-- Operatori maggiormente coinvolti nelle richieste di soccorso chiuse con risultato non totalmente positivo
+CREATE VIEW operatori_missioni_con_esito_non_totalmente_positivo AS
+SELECT o.matricola, o.nome, o.cognome, COUNT(distinct m.ID) AS conteggio_missioni
+FROM select_missioni_con_esito_non_totalmente_positivo smp
+JOIN squadra s ON smp.ID_squadra = s.ID
+JOIN squadraOperatore so ON s.ID = so.ID_squadra
+JOIN operatore o ON so.ID_operatore = o.ID
+ORDER BY conteggio_missioni DESC;
